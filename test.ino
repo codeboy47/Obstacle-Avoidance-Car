b4766a3eb5f3827ff5f22c86297e73fa2d2147f3
Akshit Singh <codeboy47@gmail.com>
2018-04-06 02:02:02 +0530

    Delete test.ino

--- test.ino
+++
@@ -1,9 +0,0 @@
-void off()
-{
-  digitalWrite(RightMotorForward, LOW);
-  digitalWrite(RightMotorReverse, LOW);
-  digitalWrite(LeftMotorForward, LOW);
-  digitalWrite(LeftMotorReverse, LOW);
-  delay(500);
-  //return;
-}
